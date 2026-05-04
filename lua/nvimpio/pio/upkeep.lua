54fc02e3e4c15cb790d5b723d46d3c1f29995cec
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-04 21:22:04 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -87,7 +87,7 @@ function M.get_clangd_unknown_args()
     '--log=error',
   }
   -- local cmd = { 'clangd', '--compile-commands-dir', './', '--check=' .. first_file, '--log=error' }
-  vim.system(cmd, { text = true, cwd = '..' }, function(obj)
+  vim.system(cmd, { text = true }, function(obj)
     -- Everything inside this function happens "later"
     if obj.code == 127 then
       vim.schedule(function()
