d22058c8d262de6dd6c4491efdfc13fe35bdc6bb
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-05 21:45:29 +0300

    update

--- plugin/nvimpio.lua
+++ plugin/nvimpio.lua
@@ -13,18 +13,18 @@ vim.api.nvim_create_user_command('Pioinit',
     desc = 'Start the PlatformIO guided setup wizard'
   }
 )
-
-if vim.fn.filereadable('platformio.ini') == 1 then
-  -- If the file is there, wake up the plugin immediately
-  require('nvimpio').setup()
-else
-  -- If not, set a one-time listener to wake up if the file appears later
-  vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
-    group = vim.api.nvim_create_augroup('PioActivation', { clear = true }),
-    pattern = 'platformio.ini',
-    callback = function()
-      require('nvimpio').setup()
-      return true -- Delete this listener once triggered
-    end,
-  })
-end
+--
+-- if vim.fn.filereadable('platformio.ini') == 1 then
+--   -- If the file is there, wake up the plugin immediately
+--   require('nvimpio').setup()
+-- else
+--   -- If not, set a one-time listener to wake up if the file appears later
+--   vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
+--     group = vim.api.nvim_create_augroup('PioActivation', { clear = true }),
+--     pattern = 'platformio.ini',
+--     callback = function()
+--       require('nvimpio').setup()
+--       return true -- Delete this listener once triggered
+--     end,
+--   })
+-- end
