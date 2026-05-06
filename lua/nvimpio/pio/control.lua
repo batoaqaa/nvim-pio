c8bdef900246cfdbb0744d9629fce1da284327a5
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-06 12:54:27 +0300

    update

--- lua/nvimpio/pio/control.lua
+++ lua/nvimpio/pio/control.lua
@@ -205,10 +205,10 @@ function M.start_watchers()
         if self.isBusy then return end
         if _G.metadata.isBusy == true then return end
         local new_hash = get_hash(self.path) or ''
+        print('ahmed:old=' .. self.last_hash .. ' new=' .. new_hash)
         if new_hash and new_hash ~= self.last_hash then
           self.last_hash = new_hash
           local env = vim.pio.get_active__env('PIO platformio.ini change: ')
-        print('ahmed:' .. env)
           if not env then return end
           _G.metadata.isBusy = true
           self.isBusy = true
