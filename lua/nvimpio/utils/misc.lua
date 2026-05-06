6058411e34f8693632e7b23f3fbc2d0b58abcb9e
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-06 21:33:56 +0300

    update

--- lua/nvimpio/utils/misc.lua
+++ lua/nvimpio/utils/misc.lua
@@ -43,7 +43,7 @@ function M.notify(msg, level)
   level = level or vim.log.levels.INFO
   local icon = icons[level] or '  '
 
-  local full_message = string.format(" %s [%s] %s", pluginName, icon, msg)
+  local full_message = string.format(" %s [%s ] %s", pluginName, icon, msg)
   -- local message = '  '.. pluginName .. ' [' .. icon .. '   ]: ' .. msg
   vim.notify(full_message, level, {
     title = pluginName,
