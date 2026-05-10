db1fa703d1904e1204fafcf55cb1a730de9591ad
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-10 12:59:44 +0300

    update

--- lua/nvimpio/clangd/commands.lua
+++ lua/nvimpio/clangd/commands.lua
@@ -5,5 +5,5 @@ vim.api.nvim_create_user_command('ClangFormatterPick', control.setFormatStyle, {
 -- ClangdCheckArgs
 vim.api.nvim_create_user_command('ClangdCheckArgs', control.getUnknownArgs, {})
 -- Clangdrestart
-vim.api.nvim_create_user_command('Clangdrestart', control.restarti, {})
+vim.api.nvim_create_user_command('Clangdrestart', control.restart, {})
 --------------------------------------------------------------------------------
