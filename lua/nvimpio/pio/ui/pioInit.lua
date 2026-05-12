3efe6513774dddb6d5508189b4aa649f2460e9c1
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-13 00:49:45 +0300

    update

--- lua/nvimpio/pio/ui/pioInit.lua
+++ lua/nvimpio/pio/ui/pioInit.lua
@@ -46,7 +46,7 @@ local function finalize_setup()
   local pio = require('nvimpio.pio.upkeep')
 
   local sample_flag = '' --wizard_data.sample == 'Yes' and ' --sample-code' or ''
-  local init_cmd = string.format('pio project init --board %s -O "framework=%s" %ss', wizard_data.board_id, wizard_data.framework, sample_flag)
+  local init_cmd = string.format('pio project init --board %s -O "framework=%s" %s', wizard_data.board_id, wizard_data.framework, sample_flag)
   local db_cmd = string.format('pio run -t compiledb -e %s', wizard_data.board_id)
   local commands = { init_cmd, db_cmd }
 
