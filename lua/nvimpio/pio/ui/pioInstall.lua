f67221a56ddd153159973b58f348880fc92a7c62
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-12 20:44:06 +0300

    update

--- lua/nvimpio/pio/ui/pioInstall.lua
+++ lua/nvimpio/pio/ui/pioInstall.lua
@@ -7,35 +7,23 @@ _G.pio_status = ''
 ------------------------------------------------------
 local function pioInstall(on_done)
   -- 1. Detect environment details
-  -- local python = OS.is_win and 'python' or 'python3'
-  --
-  -- -- 2. CORRECTED URL: Added 'raw.' prefix
-  -- local url = 'https://raw.githubusercontent.com/platformio/platformio-core-installer/master/get-platformio.py'
-  --
-  -- -- 3. Construction of the cross-platform command string
-  -- -- We use double quotes for Python's internal string to ensure compatibility with Windows cmd
-  -- local download_py = string.format("%s -c \"import urllib.request; urllib.request.urlretrieve('%s', 'get-platformio.py')\"", python, url)
-  -- local install_py = python .. ' get-platformio.py'
-  -- local full_cmd = download_py .. ' && ' .. install_py
-  --
-  -- -- Update UI status
-  -- _G.pio_status = '⏳ Installing PIO...'
-  -- vim.cmd('redrawstatus')
-  --
-  -- local pio = require('nvimpio.pio.upkeep')
-  -- pio.run_sequence({ cmnds = { full_cmd }, cb = vim.pio.handlePioInstall })
   local python = OS.is_win and 'python' or 'python3'
+
   local script = 'get-platformio.py'
+
+  -- 2. CORRECTED URL: Added 'raw.' prefix
   local script_url = 'https://raw.githubusercontent.com/platformio/platformio-core-installer/master/'
+
+  -- 3. Construction of the cross-platform commands string
   local downloald_cmd = string.format(
                            "%s -c \"import urllib.request; urllib.request.urlretrieve('%s%s', '%s')\"",
                            python, script_url, script, script)
-  local install_cmd = python .. ' get-platformio.py'
+  local install_cmd = python .. ' ' .. script
 
   local pio = require('nvimpio.pio.upkeep')
   local cb = function(status) pio.handlePioInstall(status, on_done) end
 
-  -- open toggleterm and install platformio
+  -- 4. open toggleterm and install platformio
   pio.run_sequence({ cmnds = { downloald_cmd, install_cmd }, cb = cb })
 end
 
