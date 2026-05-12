02bf3893664bd4408d26a778167544b42fe1ed0e
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-12 19:50:40 +0300

    update

--- lua/nvimpio/defConfig.lua
+++ lua/nvimpio/defConfig.lua
@@ -17,41 +17,6 @@ local defConfig = {
     { node = 'item', desc = '[T]erminal Core CLI', shortcut = 't', command = 'Piocmdf' },
     {
       node = 'menu',
-      desc = '[G]eneral',
-      shortcut = 'g',
-      items = {
-        { node = 'item', desc = '[B]uild', shortcut = 'b', command = 'Piocmdf run' },
-        { node = 'item', desc = '[C]lean', shortcut = 'c', command = 'Piocmdf run -t clean' },
-        { node = 'item', desc = '[D]evice list', shortcut = 'd', command = 'Piocmdf device list' },
-        { node = 'item', desc = '[F]ull clean', shortcut = 'f', command = 'Piocmdf run -t fullclean' },
-        { node = 'item', desc = 'git [I]gnore', shortcut = 'i', command = 'PioGitIgnore' },
-        { node = 'item', desc = '[M]onitor', shortcut = 'm', command = 'Piocmdh run -t monitor' },
-        { node = 'item', desc = '[U]pload', shortcut = 'u', command = 'Piocmdf run -t upload' },
-      },
-    },
-    {
-      node = 'menu',
-      desc = '[P]latform',
-      shortcut = 'p',
-      items = {
-        { node = 'item', desc = '[B]uild file system', shortcut = 'b', command = 'Piocmdf run -t buildfs' },
-        { node = 'item', desc = 'Program [S]ize', shortcut = 's', command = 'Piocmdf run -t size' },
-        { node = 'item', desc = '[U]pload file system', shortcut = 'u', command = 'Piocmdf run -t uploadfs' },
-        { node = 'item', desc = '[E]rase Flash', shortcut = 'e', command = 'Piocmdf run -t erase' },
-      },
-    },
-    {
-      node = 'menu',
-      desc = '[D]ependencies',
-      shortcut = 'd',
-      items = {
-        { node = 'item', desc = '[L]ist packages', shortcut = 'l', command = 'Piocmdf pkg list' },
-        { node = 'item', desc = '[O]utdated packages', shortcut = 'o', command = 'Piocmdf pkg outdated' },
-        { node = 'item', desc = '[U]pdate packages', shortcut = 'u', command = 'Piocmdf pkg update' },
-      },
-    },
-    {
-      node = 'menu',
       desc = '[A]dvanced',
       shortcut = 'a',
       items = {
@@ -75,6 +40,50 @@ local defConfig = {
     },
     {
       node = 'menu',
+      desc = '[D]ependencies',
+      shortcut = 'd',
+      items = {
+        { node = 'item', desc = '[L]ist packages', shortcut = 'l', command = 'Piocmdf pkg list' },
+        { node = 'item', desc = '[O]utdated packages', shortcut = 'o', command = 'Piocmdf pkg outdated' },
+        { node = 'item', desc = '[U]pdate packages', shortcut = 'u', command = 'Piocmdf pkg update' },
+      },
+    },
+    {
+      node = 'menu',
+      desc = '[F]lash',
+      shortcut = 'p',
+      items = {
+        { node = 'item', desc = '[B]uild file system', shortcut = 'b', command = 'Piocmdf run -t buildfs' },
+        { node = 'item', desc = 'Program [S]ize', shortcut = 's', command = 'Piocmdf run -t size' },
+        { node = 'item', desc = '[U]pload file system', shortcut = 'u', command = 'Piocmdf run -t uploadfs' },
+        { node = 'item', desc = '[E]rase Flash', shortcut = 'e', command = 'Piocmdf run -t erase' },
+      },
+    },
+    {
+      node = 'menu',
+      desc = '[G]eneral',
+      shortcut = 'g',
+      items = {
+        { node = 'item', desc = '[B]uild', shortcut = 'b', command = 'Piocmdf run' },
+        { node = 'item', desc = '[C]lean', shortcut = 'c', command = 'Piocmdf run -t clean' },
+        { node = 'item', desc = '[D]evice list', shortcut = 'd', command = 'Piocmdf device list' },
+        { node = 'item', desc = '[F]ull clean', shortcut = 'f', command = 'Piocmdf run -t fullclean' },
+        { node = 'item', desc = '[M]onitor', shortcut = 'm', command = 'Piocmdh run -t monitor' },
+        { node = 'item', desc = '[U]pload', shortcut = 'u', command = 'Piocmdf run -t upload' },
+      },
+    },
+    {
+      node = 'menu',
+      desc = '[P]latformIO',
+      shortcut = 'm',
+      items = {
+        { node = 'item', desc = '[U]pgrade PlatformIO Core', shortcut = 'u', command = 'Piocmdf upgrade' },
+        { node = 'item', desc = '[I]nstall PlatformIO Core', shortcut = 'i', command = 'PioInstall' },
+        { node = 'item', desc = '[G]it ignore', shortcut = 'g', command = 'PioGitIgnore' },
+      },
+    },
+    {
+      node = 'menu',
       desc = '[R]emote',
       shortcut = 'r',
       items = {
@@ -82,15 +91,6 @@ local defConfig = {
         { node = 'item', desc = 'Remote [T]est', shortcut = 't', command = 'Piocmdf remote test' },
         { node = 'item', desc = 'Remote [M]onitor', shortcut = 'm', command = 'Piocmdh remote run -t monitor' },
         { node = 'item', desc = 'Remote [D]evices', shortcut = 'd', command = 'Piocmdf remote device list' },
-      },
-    },
-    {
-      node = 'menu',
-      desc = '[M]iscellaneous',
-      shortcut = 'm',
-      items = {
-        { node = 'item', desc = '[U]pgrade PlatformIO Core', shortcut = 'u', command = 'Piocmdf upgrade' },
-        { node = 'item', desc = 'PlatformIO Core [I]nstall', shortcut = 'u', command = ':PioInstall' },
       },
     },
   },
