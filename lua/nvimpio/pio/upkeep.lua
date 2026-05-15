913529fb660412cda18b58d679aa35486179cc60
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-16 00:48:36 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -828,7 +828,8 @@ function M.handlePioinitDb(result, board, on_done)
     if #M.queue > 0 then
       _G.metadata.isBusy = true
       boilerplate.core_dir = _G.metadata.core_dir
-      boilerplate_gen([[platformio.ini]], vim.g.platformioRootDir)
+      -- boilerplate_gen([[platformio.ini]], vim.g.platformioRootDir)
+      boilerplate_gen([[platformio.ini]], vim.vu.cwd)
 
       trm = term.ToggleTerminal(pop(M.queue), 'float')
       if trm and on_done and type(on_done) == "function" then
