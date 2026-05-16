9b4ccd3d5af4e80b47b4f834b9c3e269aba61410
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-16 17:21:24 +0300

    update

--- lua/nvimpio/pio/ui/pioInstall.lua
+++ lua/nvimpio/pio/ui/pioInstall.lua
@@ -12,9 +12,10 @@ local function get_download_command(url, target_file)
 end
 
 --INFO: Install platformio
--- stylua: ignore start
+--- stylua: ignore start
 ------------------------------------------------------
 local function pioInstall(runtime_dir, on_done)
+  local pio = require('nvimpio.pioCheck')
   -- 1. Detect environment details
   local python = OS.is_win and 'python' or 'python3'
 
@@ -28,21 +29,21 @@ local function pioInstall(runtime_dir, on_done)
   local script_url = 'https://raw.githubusercontent.com/platformio/platformio-core-installer/master/'
 
   -- 4. Construction of the cross-platform commands string
-  local download_cmd = string.format(
-                           "%s -c \"import urllib.request; urllib.request.urlretrieve('%s%s', '%s')\"",
-                           python, script_url, script_name, script_path)
+  local download_cmd = string.format("%s -c \"import urllib.request; urllib.request.urlretrieve('%s%s', '%s')\"", python, script_url, script_name, script_path)
   -- Run the installer script out of the safe cache target space
   local install_cmd
-  local custom_penv_dir = runtime_dir .. OS.folder_sep .. "penv"
-  if OS.is_win then
-    install_cmd = string.format('$env:PLATFORMIO_PENV_DIR="%s"; %s %s --no-modify-path', custom_penv_dir, python, script_path)
-  else
-    install_cmd = string.format('PLATFORMIO_PENV_DIR="%s" %s %s --no-modify-path', custom_penv_dir, python, script_path)
-  end
+  local custom_penv_dir = pio.clean(runtime_dir .. OS.folder_sep .. 'penv')
+  -- if OS.is_win then
+  --   install_cmd = string.format('$env:PLATFORMIO_PENV_DIR="%s"; %s %s --no-modify-path', custom_penv_dir, python, script_path)
+  -- else
+  install_cmd = string.format('%sPLATFORMIO_PENV_DIR="%s" %s %s --no-modify-path', OS.is_win and '$env:' or '', custom_penv_dir, python, script_path)
+  -- end
 
   -- 5. Establish downstream update pipeline connections
-  local pio = require('nvimpio.pio.upkeep')
-  local cb = function(status) pio.handlePioInstall(status, on_done) end
+  -- local pio = require('nvimpio.pio.upkeep')
+  local cb = function(status)
+    pio.handlePioInstall(status, on_done)
+  end
 
   -- 6. open toggleterm and install platformio
   pio.run_sequence({ cmnds = { download_cmd, install_cmd }, cb = cb })
