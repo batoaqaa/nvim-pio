b5514b46b09afad9bcdd89f515b5df9609e91233
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-18 02:57:25 +0300

    update

--- lua/nvimpio/pio/control.lua
+++ lua/nvimpio/pio/control.lua
@@ -218,7 +218,7 @@ function M.init(clangd_config)
     local pio_refresh = require('nvimpio.pio.upkeep').pio_refresh
     pio_refresh(function()
       boilerplate.core_dir = _G.metadata.core_dir
-      clangd.getUnknownArgs('PIO start: ')
+      -- clangd.getUnknownArgs('PIO start: ')
       boilerplate_gen([[.clang-format]], vim.g.platformioRootDir)
       _G.metadata.isBusy = false
     end, 'PIO start: ')
