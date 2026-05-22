fc4602136435b5bc4a48ec24954011be80b4a9ce
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-22 20:38:18 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -721,11 +721,10 @@ function M.stdoutcallback(_, _, data, _)
 
       -- 4. THE GATED LOOP: Only extract AFTER the echo has passed, but BEFORE the final done token closes it
       if M.token_echo_passed and not string.find(target_text, "%.clang%-format") then
+        print(vim.inspect(clangd_extracted_args))
         for arg in string.gmatch(target_text, "unknown argument[:%s]+'([^']+)'") do
           table.insert(clangd_extracted_args, string.format('"%s"', arg:gsub('[;%.]$', '')))
         end
-        print( echo_pattern_done)
-        print('true')
         print(vim.inspect(clangd_extracted_args))
       end
     end
