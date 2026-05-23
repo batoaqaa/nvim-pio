2c593825021f591f87c56e459a3bafdf9e45d96e
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-23 23:46:17 +0300

    update

--- lua/nvimpio/pio/metadata.lua
+++ lua/nvimpio/pio/metadata.lua
@@ -122,9 +122,9 @@ _G.metadata = setmetatable({}, {
           _pio_metadata.platforms_dir = metadata.platforms_dir
           _pio_metadata.default_envs = metadata.default_envs
           _pio_metadata.envs = metadata.envs
-          vim.cmd('redrawstatus')
         end
 
+        vim.cmd('redrawstatus')
         local pio_refresh = require('nvimpio.pio.upkeep').pio_refresh
         pio_refresh(function(_)
           -- if (suscess) then require('nvimpio.clangd.control').getUnknownArgs(from) end
