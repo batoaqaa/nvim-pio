2da1d8f83ababce4b78853dc741431cfab25cdbe
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-24 22:24:07 +0300

    update

--- lua/nvimpio/clangd/control.lua
+++ lua/nvimpio/clangd/control.lua
@@ -291,7 +291,7 @@ function M.getUnknownArgsGui(from)
     --------------------------------------------------------------------------------
     -- gui
 
-    local cmd_str = string.format('%s --compile-commands-dir=. --check=%s --query-driver=%s --log=error -Wno-unknown-argument --change-cognitive-reasons=false', clangdCmd, check_file, _G.metadata.query_driver)
+    local cmd_str = string.format('%s --compile-commands-dir=. --check=%s --query-driver=%s --log=error --compile-or-cross-flags=-Wno-unknown-argument', clangdCmd, check_file, _G.metadata.query_driver)
     local pio = require('nvimpio.pio.upkeep')
     local cb = function(status)
       pio.handleClangdCheck(status, function(success, args_table)
