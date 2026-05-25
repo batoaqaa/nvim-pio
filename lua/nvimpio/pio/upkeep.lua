4e21cd74bedfef417a34490da683e82ed6e9d346
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-25 04:57:26 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -40,6 +40,9 @@ function M.get_sysroot_triplet(cc_compiler)
   -- sysroot folder is expected to have the same name as the triplet
   local sysroot = misc.normalizePath(toolchain_root .. '/' .. triplet)
   local query_driver = misc.normalizePath(bin_path .. '/' .. triplet .. '-*')
+  query_driver = OS.is_win and query_driver:gsub('/', '\\') or query_driver
+
+
 
   -- Only return data if the sysroot folder actually exists on disk
   if vim.fn.isdirectory(sysroot) == 1 then
