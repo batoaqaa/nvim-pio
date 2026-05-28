92fcc467ca976848a3bb5fcd547362aa45e7cf84
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-28 07:02:58 +0300

    Update

--- lua/nvimpio/clangd/diagnostic.lua
+++ lua/nvimpio/clangd/diagnostic.lua
@@ -134,7 +134,7 @@ function M.manage_file_diagnostics_interactive()
   snacks_api.picker({
     source = 'Microcontroller Diagnostic Mangler',
     items = picker_items,
-    layout = 'float',
+    layout = 'vscode',
     win = {
       input = {
         keys = {
