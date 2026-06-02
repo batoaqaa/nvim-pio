78c12cb5bd40dafa2e03d36ee91519c8aa638ffa
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-02 03:30:02 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -41,7 +41,8 @@ function M.get_sysroot_triplet(cc_compiler)
   -- sysroot folder is expected to have the same name as the triplet
   local sysroot = misc.normalizePath(toolchain_root .. '/' .. triplet)
 
-  local query_driver = misc.normalizePath(bin_path .. '/' .. triplet .. '-*')
+  local query_driver = misc.normalizePath(bin_path .. '/' .. triplet .. '-g*')
+  -- local query_driver = misc.normalizePath(bin_path .. '/' .. triplet .. '-*')
   -- local query_driver = misc.normalizePath(bin_path .. '/*')
 
   _G.metadata = _G.metadata or {}
