e0fe133871d37b162cfee5a9a2495ba6739ae934
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-02 05:35:01 +0300

    update

--- lua/nvimpio/clangd/control.lua
+++ lua/nvimpio/clangd/control.lua
@@ -91,18 +91,11 @@ function M.getClangdConfig()
   --   end
   -- end
   --
-
-  -- 1. Use glob to find the actual paths matching your wildcards
-  local path = _G.metadata.query_driver .. (OS.is_win and '.exe' or '')
-  print(path)
-  local matches = vim.fn.glob(_G.metadata.query_driver .. (OS.is_win and '.exe' or ''), true, true)
-
-  if #matches > 0 then
-    -- 2. Take the first valid match and normalize it
-    q_driver = vim.fs.normalize(matches[1])
+  if _G.metadata.query_driver and _G.metadata.query_driver ~= '' then
+    q_driver = _G.metadata.query_driver
   else
-    -- Fallback if no matching compiler path is found
-    q_driver = _G.metadata.core_dir .. '/*/packages/toolchain-*/bin/*' .. OS.is_win and '.exe' or ''
+    -- Standard global default sandbox path fallback option if initialization variables are blank
+    q_driver = vim.fs.normalize(_G.metadata.core_dir .. '/*/packages/toolchain-*/bin/*' .. (OS.is_win and '.exe' or ''))
   end
 
   -- Format your template string
