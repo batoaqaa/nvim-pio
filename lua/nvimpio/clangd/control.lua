44428b23c520503fad27b31ff48318fe2b2c88eb
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-02 05:24:24 +0300

    update

--- lua/nvimpio/clangd/control.lua
+++ lua/nvimpio/clangd/control.lua
@@ -93,7 +93,7 @@ function M.getClangdConfig()
   --
 
   -- 1. Use glob to find the actual paths matching your wildcards
-  local path = _G.metadata.query_driver .. OS.is_win and '.exe' or ''
+  local path = _G.metadata.query_driver .. (OS.is_win and '.exe' or '')
   print(path)
   local matches = vim.fn.glob(_G.metadata.query_driver .. OS.is_win and '.exe' or '', true, true)
 
