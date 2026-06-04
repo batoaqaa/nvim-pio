f22b9a08fe753dda52e7e6503fd538e242ac6432
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-04 20:09:22 +0300

    update

--- lua/nvimpio/defConfig.lua
+++ lua/nvimpio/defConfig.lua
@@ -12,7 +12,7 @@ local defConfig = {
   menu_name = 'PlatformIO', -- replace this menu name to your convenience
 
   menu_bindings = {
-    { node = 'item', desc = '[B]lock diagnostic', shortcut = 'b', command = 'ClangdDiagnosticBlock' },
+    { node = 'item', desc = '[B]lock diagnostic', shortcut = 'b', command = 'ClangdFilter' },
     { node = 'item', desc = 'Switch [E]nv', shortcut = 'e', command = 'PioPickEnv' },
     { node = 'item', desc = '[I]nitiate project', shortcut = 'i', command = 'Pioinit' },
     { node = 'item', desc = '[L]ist terminals', shortcut = 'l', command = 'PioTermList' },
