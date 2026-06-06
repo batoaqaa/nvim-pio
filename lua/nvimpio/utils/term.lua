afd16b508acac6cf3bf83dabb388ac4ad1d1b6b5
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-06 18:08:19 +0300

    update

--- lua/nvimpio/utils/term.lua
+++ lua/nvimpio/utils/term.lua
@@ -38,14 +38,14 @@ local function getPreviousWindow(orig_window)
   ---@field term Terminal|nil  -- Handle for horizontal terminal
   ---@field mon Terminal|nil
   ---@field cli Terminal|nil
-  ---@field float boolean      -- flag float terminal
+  ---@field tab boolean      -- flag tab terminal
   ---@field orig_window number|nil
   local prev = {
     orig_window = orig_window,
     term = nil, --active terminal
     cli = nil, --cli terminal
     mon = nil, --mon terminal
-    float = false, --is active terminal direction float
+    tab = false, --is active terminal direction tab
   }
   local terms = require('toggleterm.terminal').get_all(true)
   if #terms ~= 0 then
@@ -59,8 +59,8 @@ local function getPreviousWindow(orig_window)
             prev.orig_window = tonumber(name_splt[2]) -- set orig_window to the previous terminal onrig_window
             prev.term = terms[i]
           end
-          if terms[i].direction == 'float' then
-            prev.float = true
+          if terms[i].direction == 'tab' then
+            prev.tab = true
           end
         elseif name_splt[1] == 'piomon' then
           prev.mon = terms[i]
@@ -69,8 +69,8 @@ local function getPreviousWindow(orig_window)
             prev.orig_window = tonumber(name_splt[2]) -- set orig_window to the previous terminal onrig_window
             prev.term = terms[i]
           end
-          if terms[i].direction == 'float' then
-            prev.float = true
+          if terms[i].direction == 'tab' then
+            prev.tab = true
           end
         end
       end
@@ -317,7 +317,7 @@ function M.ToggleTerminal(command, direction)
 
   -- INFO: create new terminal
   local terminal = require('toggleterm.terminal').Terminal:new(termConfig)
-  if prev.term and prev.float then
+  if prev.term and prev.tab then
     prev.term.close()
   end
   terminal:toggle()
