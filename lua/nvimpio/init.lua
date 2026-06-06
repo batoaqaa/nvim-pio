a0ff1ba7aace74e83bf71ffdf45b3791a3e04b40
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-06 16:33:27 +0300

    update

--- lua/nvimpio/init.lua
+++ lua/nvimpio/init.lua
@@ -4,7 +4,8 @@
 local original_require = _G.require
 _G.require = function(modname)
   -- Intercept right when the metadata background runner is pulled into memory
-  if modname == 'nvimpio.pio.metadata' or modname == 'nvimpio.pio.upkeep' then
+  -- if modname == 'nvimpio.pio.metadata' or modname == 'nvimpio.pio.upkeep' then
+  if modname == 'nvimpio.pio.metadata' or modname == 'nvimpio.pio.metadata' then
     print('\n[CRITICAL HOOK] DETECTED WHO REQUIRED: ' .. modname)
     print(debug.traceback())
     print('==================================================\n')
