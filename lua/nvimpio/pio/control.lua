77ca164af9619c41b1d742aa0d9d4fc8b2554bb1
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-07 21:48:10 +0300

    update

--- lua/nvimpio/pio/control.lua
+++ lua/nvimpio/pio/control.lua
@@ -8,6 +8,60 @@ local clangdRestart = clangd.clangdRestart
 local boilerplate = require('nvimpio.boilerplate')
 local boilerplate_gen = boilerplate.boilerplate_gen
 
+local state = {
+    floating = {
+        buf = -1,
+        win = -1,
+    },
+}
+
+local function create_floating_window(opts)
+    opts = opts or {}
+    local width = opts.width or math.floor(vim.o.columns * 0.9)
+    local height = opts.height or math.floor(vim.o.lines * 0.9)
+
+    -- Calculate the position to center the window
+    local col = math.floor((vim.o.columns - width) / 2)
+    local row = math.floor((vim.o.lines - height) / 2)
+
+    -- Create a buffer
+    local buf = nil
+    if vim.api.nvim_buf_is_valid(opts.buf) then
+        buf = opts.buf
+    else
+        buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+    end
+
+    -- Define window configuration
+    local win_config = {
+        relative = "editor",
+        width = width,
+        height = height,
+        col = col,
+        row = row,
+        style = "minimal",
+        border = "rounded",
+    }
+
+    local win = vim.api.nvim_open_win(buf, true, win_config)
+
+    return { buf = buf, win = win }
+end
+
+local toggle_terminal = function()
+    if not vim.api.nvim_win_is_valid(state.floating.win) then
+        state.floating = create_floating_window({ buf = state.floating.buf })
+        if vim.bo[state.floating.buf].buftype ~= "terminal" then
+            vim.cmd.terminal()
+        end
+        -- unlist the terminal from buffer list
+        vim.api.nvim_set_option_value("buflisted", false, { buf = state.floating.buf })
+        vim.cmd("startinsert")
+    else
+        vim.api.nvim_win_hide(state.floating.win)
+    end
+end
+
 --INFO:
 --=============================================================================
 --  watchers setup
@@ -264,14 +318,8 @@ function M.init(clangd_config)
 
 local term_buf = nil
 
-vim.keymap.set({'n', 't'}, '<leader>st', function()
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0,5)
-end, { desc = "Toggle true full-buffer terminal" })
 
-
+vim.keymap.set({ "n", "t" }, "<leader>st", toggle_terminal, { desc = "Toggle terminal" })
 
 vim.keymap.set({'n', 't'}, '<leader>tf', function()
   -- 1. Safely exit terminal mode if you are currently typing in it
