2e43f219bb44fcfc79316c420ada0882f032cedc
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-09 00:49:30 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -668,7 +668,19 @@ local pio_buffer = ''
 local content = ''
 
 -- function M.stdoutcallback(_, _, data, _)
-function M.stdoutcallback(_, data, _)
+function M.stdoutcallback(_, data, event)
+  -----------------------------------------------------------------------------
+  -- 🔍 DETECTIVE TRACE HOOKS: Run this snippet to verify active process triggers
+  -----------------------------------------------------------------------------
+  -- Trace Call #1: Logs the unique event name ("stdout") and incoming table length
+  vim.notify(string.format('[PioTrace] Callback hit! Event: %s, Chunks received: %d', tostring(event), data and #data or 0), vim.log.levels.INFO)
+
+  -- Trace Call #2: Dumps the raw log string block content to verify stream integrity
+  if data and #data > 0 then
+    vim.notify('[PioData Dump]: ' .. table.concat(data, ' | '), vim.log.levels.DEBUG)
+  end
+  -----------------------------------------------------------------------------
+
   if not data or #data == 0 then
     return
   end
