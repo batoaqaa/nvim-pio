2860ed2fadb86d6cbd7b594cdd53658e8987a0e8
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-10 21:25:47 +0300

    update

--- lua/nvimpio/device/terminal.lua
+++ lua/nvimpio/device/terminal.lua
@@ -88,11 +88,10 @@ function Terminal:send(command)
   end
 
   if not state.job or state.job <= 0 then return end
-  if cmd_str ~= "" then
-    vim.fn.chansend(state.job, self.newline)
-  end
+  -- if cmd_str ~= "" then
+  --   vim.fn.chansend(state.job, self.newline)
+  -- end
   vim.fn.chansend(state.job, cmd_str .. self.newline)
-  vim.fn.chansend(state.job, cmd_str)
 end
 
 -- Hard stop background processes loops safely
