2a55017c1cdb9e7a31cdec3a8c8d29f3e3d16c45
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-10 21:42:56 +0300

    update

--- lua/nvimpio/device/terminal.lua
+++ lua/nvimpio/device/terminal.lua
@@ -168,7 +168,7 @@ function Terminal:show()
   pcall(vim.api.nvim_set_option_value, "winfixheight", true, { scope = "local", win = state.win })
   M.UpdateWinbarTitles()
 
-  vim.cmd("startinsert")
+  -- vim.cmd("startinsert")
   return true
 end
 
