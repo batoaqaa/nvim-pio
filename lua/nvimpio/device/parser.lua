54ea30291553098c52e8a49a3d384c45f896afc2
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-12 05:33:04 +0300

    update

--- lua/nvimpio/device/parser.lua
+++ lua/nvimpio/device/parser.lua
@@ -166,7 +166,7 @@ end
 -- Handle after pioinit execution
 -- *=============================================================================
 function M.cleanSequencer()
-  if require('nvimpio').is_active then _G.metadata.isBusy = false end
+  if _G.metadata then _G.metadata.isBusy = false end
   require('nvimpio.device.terminal').stdout_callback = nil -- Careful: make sure this doesn't break other terms
 end
 
