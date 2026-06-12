3565f69cb89a0d11e209d107f81faab1240f5754
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-12 20:17:24 +0300

    update

--- nvimpio.lua
+++ nvimpio.lua
@@ -275,7 +275,7 @@ local plugins = {
                 -- 2. Clean up any broken references and append the uncrashable proxy to your statusline
                 -- get_nvimpio_status()
                 -- return pcall(require, 'nvimpio.statusline') and require('nvimpio.statusline').get_status_string()
-                -- return require('nvimpio.statusline').get_status_string()
+                return require('nvimpio.statusline').get_status_string()
               end,
               'filetype',
             },
