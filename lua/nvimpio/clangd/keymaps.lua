99c8fd4f6384e1baa3916616cf79161ea655fd42
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-18 08:33:08 +0300

    update

--- lua/nvimpio/clangd/keymaps.lua
+++ lua/nvimpio/clangd/keymaps.lua
@@ -75,7 +75,14 @@ function K.lspKeymaps(client, bufnr)
   end
   if client:supports_method('workspace/symbol') then
     -- if client.server_capabilities.workspaceSymbolProvider then
-    bufkeymap('n', 'glww', vim.lsp.buf.workspace_symbol, 'List [w]orkspace symbols')
+    -- bufkeymap('n', 'glww', vim.lsp.buf.workspace_symbol, 'List [w]orkspace symbols')
+
+    bufkeymap('n', 'glww', function()
+      require('telescope.builtin').lsp_dynamic_workspace_symbols({
+        symbols = { 'function', 'method' }, -- Pre-filter to only show functions and methods
+        query = '',
+      })
+    end, 'Find [W]orkspace Symbols (Functions)')
     -- bufkeymap('n', 'glww', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
   end
   if client.server_capabilities.workspace then
