cc305020951fa14155fbccd668fc6e8b46072fe9
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-22 05:00:00 +0300

    update

--- lua/nvimpio/clangd/control.lua
+++ lua/nvimpio/clangd/control.lua
@@ -85,7 +85,7 @@ function M.getClangdConfig()
 
   local _, count = json_config:gsub('%%s', '')
   -- Only use string.format if there is one or less %s
-  if count <= 1 then
+  if count <= 2 then
     merged_json = string.format(json_config or '', OS.project_dir, q_driver)
   end
 
