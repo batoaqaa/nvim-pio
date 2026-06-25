93b8dfb4de00d8c9c61de5318370c70457ef65cb
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-26 01:08:45 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -538,7 +538,6 @@ CompileFlags:
       table.concat(formattedHppAdd, ',\n    ')  -- formattedHppAdd
     )
     local final_content = staticBlock .. '\n' .. dynamicBlock
-    print(final_content)
 
     -- 4. UNCONDITIONAL DISK WRITER MATRIX
     local read_ok, old_content = misc.readFile(cwdClangd)
