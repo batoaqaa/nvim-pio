33a3215dd2f1ecb234a1b69fbad8a57da96e41bd
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-27 17:16:55 +0300

    update

--- lua/nvimpio/clangd/control.lua
+++ lua/nvimpio/clangd/control.lua
@@ -91,8 +91,8 @@ function M.getClangdConfig()
   if count <= 2 then
     -- merged_json = string.format(json_config or '', q_driver)
     -- merged_json = string.format(json_config or '', OS.project_dir, q_driver, table.concat(formatted_fallbackFlags, ','))
-    merged_json = string.format(json_config or '', OS.project_dir, q_driver)
-    -- merged_json = string.format(json_config or '', OS.nvimpio_config_dir, q_driver)
+    -- merged_json = string.format(json_config or '', OS.project_dir, q_driver)
+    merged_json = string.format(json_config or '', OS.nvimpio_config_dir, q_driver)
   end
 
   -- 'decode' converts JSON string -> Lua table
