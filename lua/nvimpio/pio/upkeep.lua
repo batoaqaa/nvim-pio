abfa85987ee29c0094b476327cf58bc622a1da11
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-28 20:04:28 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -567,7 +567,9 @@ fetch_metadata = function(callback, active_env, from, attempts)
           if idok and (content ~= '') then
             _G.metadata.framework = _G.metadata.envs[active_env].framework
 
-local pattern = string.format("^(.-framework%%-%s[^/\\\\]*)", _G.metadata.framework)
+-- Safe cross-platform Lua pattern
+   local pattern = string.format('([A-Za-z]:[^/\"]-[/\\]%%.platformio[/\\].-[/\\]packages[/\\]framework%%-%s[^/\\\"]-)[/\\]', _G.metadata.framework)
+-- local pattern = string.format("^(.-framework%%-%s[^/\\\\]*)", _G.metadata.framework)
 
 
             -- local pattern = string.format('([A-Za-z]:[^\"]-/%%.platformio/.-packages/framework%%-%s[^/\\\"]-)/', _G.metadata.framework)
@@ -599,7 +601,8 @@ local pattern = string.format("^(.-framework%%-%s[^/\\\\]*)", _G.metadata.framew
   -- end
   elseif idok and content and content ~= '' then
     _G.metadata.framework = _G.metadata.envs[active_env].framework
-local pattern = string.format("^(.-framework%%-%s[^/\\\\]*)", _G.metadata.framework)
+   local pattern = string.format('([A-Za-z]:[^/\"]-[/\\]%%.platformio[/\\].-[/\\]packages[/\\]framework%%-%s[^/\\\"]-)[/\\]', _G.metadata.framework)
+-- local pattern = string.format("^(.-framework%%-%s[^/\\\\]*)", _G.metadata.framework)
 
 
   -- local pattern_template = [=[([A-Za-z]:[^"]-/%.platformio/.-packages/framework%%-%s[^/"]-)/]=]
