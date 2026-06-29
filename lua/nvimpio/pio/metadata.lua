581e82cccd075b16af3be998e13f9b02f0436554
batoaqaa <ahmed.batoaq@gmail.com>
2026-06-29 10:16:51 +0300

    update

--- lua/nvimpio/pio/metadata.lua
+++ lua/nvimpio/pio/metadata.lua
@@ -89,6 +89,7 @@ _G.metadata = setmetatable({}, {
   __newindex = function(_, key, value)
     -- Guard: Skip execution if the new value is identical to the current state
     if key == 'active_env' then
+      print('here')
       OS.clangd_filter = vim.fs.joinpath(OS.nvimpio_config_dir, value, OS.clangdfilter)
     end
     if _pio_metadata[key] == value then return end -- Performance check
