9199d8e5c50a01eaa83044061ab6cd013750afd9
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-04 08:06:01 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -219,7 +219,7 @@ function M.readContent(tbl)
       -- Glue the text blocks before and after this specific project's sandbox together
       -- content = content:sub(1, start_idx - 1) .. tbl:block() .. content:sub(actual_end)
     else
-      content = content .. (content ~= "" and not content:match("\n$") and "\n" or "") .. tbl:block() .. "\n"
+      content = content .. (content ~= "" and not content:match("\n$") and "\n" or "") .. tbl:block()
       -- content = ''
       -- if content ~= "" and not content:match("\n$") then content = content .. "\n" end
     end
