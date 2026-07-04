b7c4abaaa250c2a4d7085e72012fb8a0c7a1474e
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-04 10:05:47 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -358,7 +358,7 @@ CompileFlags:
       -- 4. Escape every literal dot inside the folders completely dynamically
       -- path = path:gsub("%.%w+", [[\%0]])  -- this only for passing string.format()
       path = path:gsub("%.", ".")  -- this only for passing string.format()
-      path = path:gsub("%.", "[/\\]")  -- this only for passing string.format()
+      path = path:gsub("/", "[/\\]")  -- this only for passing string.format()
       -- path = path:gsub("(%%.)", "\\\\%%1") -- this for everything else
       -- 5. Recombine them seamlessly without a trailing slash
       return drive .. path
