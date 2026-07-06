1b41d4dc2725a5e0315c0a24355377ca3477132c
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-06 14:27:27 +0300

    update

--- lua/nvimpio/pio/upkeep.lua
+++ lua/nvimpio/pio/upkeep.lua
@@ -785,8 +785,9 @@ fetch_metadata = function(callback, active_env, from, attempts)
     -- python -c "import subprocess; res = subprocess.run(['pio', 'project', 'metadata', '-e', 'olimex_h407', '--json-output'], 
     --capture_output=True, text=True, encoding='utf-8'); open('C:/Users/batoaqaa/AppData/Local/ahmed/test3/.nvimpio/olimex_h407/idedata.json', 'w', encoding='utf-8').write(res.stdout)"
 
-    local idecmd = string.format('pio project metadata -e %s --json-output > "%s"', active_env, idedata_file )
-    -- local idecmd = string.format('pio run -t idedata -e %s -s', active_env)
+    -- local idecmd = string.format('pio project metadata -e %s --json-output > "%s"', active_env, idedata_file )
+    local idecmd = string.format('pio run -t idedata -e %s -s', active_env)
+
     -- local idecmd = string.format('pio project metadata -e %s --json-output-path "%s"', active_env, idedata_file )
     -- local runcmd = string.format('pio run -e %s', active_env)
 
