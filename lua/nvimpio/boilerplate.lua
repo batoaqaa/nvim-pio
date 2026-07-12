049054d5922d1a5eb3211c364b0e2c7f50198451
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-12 20:12:43 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -91,9 +91,9 @@ boilerplate['.clangdConfig.json'] = {
     ".git"
   ],
   "init_options": {
-      compilationDatabase = {
-        directory =%s,
-      }
+    "compilationDatabase": [
+        "directory=%s",
+    ],
     "clangdFileStatus": true,
     "completeUnimported": true,
     "fallbackFlags": [
