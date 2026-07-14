4b6838a51bcc42d9232c77ae48b8d60fa2e05a47
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-14 22:00:40 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -202,7 +202,6 @@ boilerplate['.clangd'] = {
 If:
   PathMatch: ['%s/.*']
 CompileFlags:
-  CompilationDatabase: "%s"
   BuiltinHeaders: QueryDriver
   Remove: [%s]
   Add: [%s]
@@ -412,7 +411,7 @@ CompileFlags:
           return string.format( self.Global,
                 ref.start_marker,
                 clean_packages_dir,                                -- If: PathMatch: ['%s/.*']
-                OS.project_dir,                                    -- CompilationDatabase: "%s"
+                -- OS.project_dir,                                    -- CompilationDatabase: "%s"
                 table.concat(formatted_remove_ASSEMBLY, ',\n    '),--   Remove: [%s]
                 table.concat(formattedGlobAdd, ',\n    '),             --   Add: [%s]
                 ref.end_marker)
