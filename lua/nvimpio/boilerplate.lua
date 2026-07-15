fbb38ef75ea3f4e88670581ffaf913e54afaa88e
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-15 14:15:38 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -200,7 +200,7 @@ boilerplate['.clangd'] = {
 %s
 ---
 If:
-  PathMatch: ['%s/.*[.]h']
+  PathMatch: ['%s/.*[.]h$']
 CompileFlags:
   BuiltinHeaders: QueryDriver
   Add: [%s]
@@ -423,7 +423,7 @@ CompileFlags:
       -- 3. Extract the Windows drive letter if it exists
       local drive, main_path = path:match("^(%a:)(.*)$")
       if drive then
-        drive = drive:lower()
+        drive = '[' .. drive:lower() .. drive:uppeer() .. ']'
         path = main_path
       else drive = "" end -- Linux/macOS
       -- 4. Escape every literal dot inside the folders completely dynamically
