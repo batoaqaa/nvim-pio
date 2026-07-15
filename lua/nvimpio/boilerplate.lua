820bfd2ac9eb6a30ad729e56954931febee74f84
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-15 07:02:24 +0300

    update

--- lua/nvimpio/boilerplate.lua
+++ lua/nvimpio/boilerplate.lua
@@ -355,13 +355,13 @@ CompileFlags:
     ------------------------------------------------------------------------------
     local formattedGlobAdd = is_cpp and { '"-xc++"',
                                     '"-std=gnu++17"',
-                                    string.format('"-I%s/src"', OS.project_dir),
-                                    -- string.format('"--include=%s/src/main.cpp"', OS.project_dir)
+                                    -- string.format('"-I%s/src"', OS.project_dir),
+                                    string.format('"--include=%s/src/mainx.cpp"', OS.project_dir)
                                   }
                                     or { '"-xc"',
                                     '"-std=gnu17"',
-                                    string.format('"-I%s/src"', OS.project_dir),
-                                    -- string.format('"--include=%s/src/main.c"', OS.project_dir)
+                                    -- string.format('"-I%s/src"', OS.project_dir),
+                                    string.format('"--include=%s/src/mainx.c"', OS.project_dir)
                                   }
     ------------------------------------------------------------------------------
 
