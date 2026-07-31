ce5704bb7aaafbec61b0178dadc8459e294f2791
batoaqaa <ahmed.batoaq@gmail.com>
2026-07-31 22:26:28 +0300

    update

--- lua/nvimpio/clangd/diagnostic.lua
+++ lua/nvimpio/clangd/diagnostic.lua
@@ -195,7 +195,7 @@ function M.clean_file_path_pipeline(result)  -- change pio flags/codes  --> writ
         end
       end
     elseif is_pio then
-      -- print('2: ' .. code)
+      print('2: ' .. code)
       if (code and blocked_pckg_codes[code]) then show_diagnostics = false
       elseif autoPckg then
         -- Suppress diagnostics inside the pio framework root
